0acb2140876461fedf5811cde0c2391f8c763f08
Rodrigo Oliveira <rodrigo.oliveira@compasso.com.br>
2023-04-20 09:09:48 -0300

    Adição do uso de camera

--- app-1/src/components/CounterAppOne.tsx
+++ app-1/src/components/CounterAppOne.tsx
@@ -1,11 +1,13 @@
 import { Text, Button, Flex } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import Webcam from "react-webcam";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
     <Flex gap="1rem" direction="column">
+      <Webcam />
       <Text>
         Add by one each click <strong>APP-1</strong>
       </Text>
